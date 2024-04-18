720b8c046d7a3828c67ae0e9e7012285260adef2
Kennedy Martins <kemartins@gmail.com>
2024-04-18 16:25:03 -0300

    Implementando funcionalidade no campo Busca Pokemon

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -6,21 +6,29 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss'],
 })
+
 export class PokeListComponent implements OnInit {
   
+  private setAllPokemons: any;
   public getAllPokemons: any;
 
-  constructor(
-    private pokeApiService: PokeApiService
-  ) {}
 
-  ngOnInit(): void {
-    // Navegar pela Url (verificar com console.log)
-    this.pokeApiService.apiListAllPokemons.subscribe(
-      res => {
-        this.getAllPokemons = res.results;
-        //console.log(this.getAllPokemons);
-      }
-    );
+  constructor(private pokeApiService: PokeApiService) {}
+
+  ngOnInit(): void { // Navegar pela Url (verificar com console.log)
+    this.pokeApiService.apiListAllPokemons.subscribe((res) => {
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
+      //console.log(this.getAllPokemons);
+    });
+  }
+
+  public getSearch(value: string) {
+    //console.log(value);
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 }
