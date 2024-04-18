fe6493831a6dbef4150855555e0ebf9971054d09
Kennedy Martins <kemartins@gmail.com>
2024-04-18 10:48:49 -0300

    Refatorando chamada da API

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -2,25 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 //Observable
-import { Observable, tap } from 'rxjs'; 
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokeApiService {
-
   private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  get apiListAllPokemons():Observable<any> { // Observable é opcional pois já vem enbutido
+  get apiListAllPokemons(): Observable<any> { // Observable e opcional pois já vem enbutido
     return this.http.get<any>(this.url).pipe(
-      tap(res => res),
-      tap( res => {
-        console.log(res)
+      tap((res) => res),
+      tap((res) => {
+        res.results.map((resPokemons: any) => { // esse map e do javascript e não do rxjs
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          );
+        });
       })
-    )
+    );
+  }
+
+  public apiGetPokemons(url: string): Observable<any> { //Observable opcional
+    return this.http.get<any>(url).pipe(
+      map( // esse map e do rxjs
+        (res) => res
+      )
+    );
   }
 }
